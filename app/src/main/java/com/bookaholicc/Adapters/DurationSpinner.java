c2499edc4cd29853734e27c71f78b1e96d7624e3
nanborklabs <nandhu12195@gmail.com>
2017-07-24 22:17:54 +0530

    Woring on Cart Spinner

---
+++ app/src/main/java/com/bookaholicc/Adapters/DurationSpinner.java
@@ -0,0 +1,8 @@
+package com.bookaholicc.Adapters;
+
+/**
+ * Created by nandhu on 24/7/17.
+ */
+
+public class DurationSpinner {
+}
---
+++ app/src/main/java/com/bookaholicc/Model/ExamModel.java
@@ -0,0 +1,8 @@
+package com.bookaholicc.Model;
+
+/**
+ * Created by nandhu on 24/7/17.
+ */
+
+public class ExamModel {
+}
